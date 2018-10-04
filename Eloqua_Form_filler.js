8a9c68e533bddd1eed801876fd32dc8520070ac7
Nils Kummert <n.kummert@gmail.com>
2018-10-04 14:52:17 +0200

    Added export

--- Eloqua_Form_filler.js
+++ Eloqua_Form_filler.js
@@ -8,29 +8,33 @@ function getRandomArbitrary(min, max) {
   }
 }
 
-let form = document.querySelector('form.eloquaForm');
-let inputs = Array.from(form.querySelectorAll('input'));
-inputs = inputs.concat(Array.from(form.querySelectorAll('select')));
+function fillOut(){
+	let form = document.querySelector('form.eloquaForm');
+	let inputs = Array.from(form.querySelectorAll('input'));
+	inputs = inputs.concat(Array.from(form.querySelectorAll('select')));
 
-for(let i = 0; i< inputs.length; i++){
-	let type = inputs[i].type;
-	if(type === 'text'){
-		inputs[i].value = "ELTest";
-	}
-	else if(type === 'email'){
-		inputs[i].value = "ElTest@eron.de"
-	}
-	else if(type === 'select-one'){
-		let selected = inputs[i].options[inputs[i].options.length-1];
-		inputs[i].value = selected.getAttribute('value');
-		let parentChildren = inputs[i].parentElement.children;
-		for(let j = 0; j <parentChildren.length; j++){
-			if(parentChildren[j].classList.contains('MMM--fancyDropAnchor')){
-				parentChildren[j].firstChild.textContent = selected.text;
+	for(let i = 0; i< inputs.length; i++){
+		let type = inputs[i].type;
+		if(type === 'text'){
+			inputs[i].value = "ELTest";
+		}
+		else if(type === 'email'){
+			inputs[i].value = "ElTest@eron.de"
+		}
+		else if(type === 'select-one'){
+			let selected = inputs[i].options[inputs[i].options.length-1];
+			inputs[i].value = selected.getAttribute('value');
+			let parentChildren = inputs[i].parentElement.children;
+			for(let j = 0; j <parentChildren.length; j++){
+				if(parentChildren[j].classList.contains('MMM--fancyDropAnchor')){
+					parentChildren[j].firstChild.textContent = selected.text;
+				}
 			}
 		}
-	}
-	else if(type === "checkbox"){
-		inputs[i].checked = getRandomArbitrary(1,2);
-	}
+		else if(type === "checkbox"){
+			inputs[i].checked = getRandomArbitrary(1,2);
+		}
+	}	
 }
+
+export {getRandomArbitrary, fillOut}
