fcaa4734232a7407449fb5cd22b13e649e9bfa7f
Nils Kummert <n.kummert@gmail.com>
2018-10-04 14:31:26 +0200

    Add files via upload
    Init

---
+++ Eloqua_Form_filler.js
@@ -0,0 +1,36 @@
+function getRandomArbitrary(min, max) {
+  let o = Math.round(Math.random() * (max - min) + min);
+  if(o===1){
+  	return false;
+  }
+  else{
+  	return true;
+  }
+}
+
+let form = document.querySelector('form.eloquaForm');
+let inputs = Array.from(form.querySelectorAll('input'));
+inputs = inputs.concat(Array.from(form.querySelectorAll('select')));
+
+for(let i = 0; i< inputs.length; i++){
+	let type = inputs[i].type;
+	if(type === 'text'){
+		inputs[i].value = "ELTest";
+	}
+	else if(type === 'email'){
+		inputs[i].value = "ElTest@eron.de"
+	}
+	else if(type === 'select-one'){
+		let selected = inputs[i].options[inputs[i].options.length-1];
+		inputs[i].value = selected.getAttribute('value');
+		let parentChildren = inputs[i].parentElement.children;
+		for(let j = 0; j <parentChildren.length; j++){
+			if(parentChildren[j].classList.contains('MMM--fancyDropAnchor')){
+				parentChildren[j].firstChild.textContent = selected.text;
+			}
+		}
+	}
+	else if(type === "checkbox"){
+		inputs[i].checked = getRandomArbitrary(1,2);
+	}
+}
